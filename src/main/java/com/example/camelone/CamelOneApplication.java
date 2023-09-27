b66021ca576b67f2f30e19c5a78c8782bac30469
Yohannes Tesfaye <joguyjohna@gmail.com>
2023-09-27 09:10:45 +0300

    initial commit

---
+++ src/main/java/com/example/camelone/CamelOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.camelone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/camelone/CamelOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camelone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
